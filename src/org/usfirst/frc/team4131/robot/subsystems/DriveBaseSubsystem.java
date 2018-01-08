7ec91d77317d4d3d0df13e47ec039f8d79a6d386
AgentTroll <woodyc40@gmail.com>
2018-01-08 15:16:13 -0800

    Switch from CANTalon to TalonSRX drive controller

--- src/org/usfirst/frc/team4131/robot/subsystems/DriveBaseSubsystem.java
+++ src/org/usfirst/frc/team4131/robot/subsystems/DriveBaseSubsystem.java
@@ -1,23 +1,22 @@
 package org.usfirst.frc.team4131.robot.subsystems;
 
-import com.ctre.CANTalon;
 import com.ctre.phoenix.motorcontrol.ControlMode;
-
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import org.usfirst.frc.team4131.robot.RobotMap;
 import org.usfirst.frc.team4131.robot.commands.MoveCommand;
 
 public class DriveBaseSubsystem extends Subsystem {
-    private final CANTalon l1;
-    private final CANTalon l2;
-    private final CANTalon r1;
-    private final CANTalon r2;
+    private final TalonSRX l1;
+    private final TalonSRX l2;
+    private final TalonSRX r1;
+    private final TalonSRX r2;
 
     public DriveBaseSubsystem() {
-        this.l1 = new CANTalon(RobotMap.L1);
-        this.l2 = new CANTalon(RobotMap.L2);
-        this.r1 = new CANTalon(RobotMap.R1);
-        this.r2 = new CANTalon(RobotMap.R2);
+        this.l1 = new TalonSRX(RobotMap.L1);
+        this.l2 = new TalonSRX(RobotMap.L2);
+        this.r1 = new TalonSRX(RobotMap.R1);
+        this.r2 = new TalonSRX(RobotMap.R2);
     }
 
     @Override
@@ -25,10 +24,14 @@ public class DriveBaseSubsystem extends Subsystem {
         this.setDefaultCommand(new MoveCommand(this));
     }
 
+    // TODO: Figure out the correct control mode on an
+    // actual robot
+    private static final ControlMode CTL = ControlMode.PercentOutput;
+
     public void doMove(double l, double r) {
-        this.l1.set(ControlMode.Velocity, l);
-        this.l2.set(ControlMode.Velocity, l);
-        this.r1.set(ControlMode.Velocity, -r);
-        this.r2.set(ControlMode.Velocity, -r);
+        this.l1.set(CTL, l);
+        this.l2.set(CTL, l);
+        this.r1.set(CTL, -r);
+        this.r2.set(CTL, -r);
     }
 }
