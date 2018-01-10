8a49f712702f00ccc53578d67092dae61f7818fd
AgentTroll <woodyc40@gmail.com>
2018-01-09 16:49:06 -0800

    Adds some interval test code

--- src/org/usfirst/frc/team4131/robot/commands/AutoDefaultCommand.java
+++ src/org/usfirst/frc/team4131/robot/commands/AutoDefaultCommand.java
@@ -4,6 +4,7 @@ import org.usfirst.frc.team4131.robot.subsystems.DriveBaseSubsystem;
 
 public class AutoDefaultCommand extends SingleSubsystemCmd<DriveBaseSubsystem> {
     private long start = -1;
+    private long last;
 
     public AutoDefaultCommand(DriveBaseSubsystem subsystem) {
         super(subsystem);
@@ -12,10 +13,15 @@ public class AutoDefaultCommand extends SingleSubsystemCmd<DriveBaseSubsystem> {
     @Override
     protected void initialize() {
         this.start = System.currentTimeMillis();
+        this.last = System.currentTimeMillis();
     }
 
     @Override
     protected void execute() {
+        long time = System.currentTimeMillis();
+        System.out.println("ELAPSED INT: " + (time - this.last));
+        this.last = time;
+
         this.subsystem.doMove(1, 1);
     }
 
