ad067bf61c5362b00986cc079e5ff9273b686afb
AgentTroll <woodyc40@gmail.com>
2018-01-11 17:36:59 -0800

    Prepare FMS line

--- src/org/usfirst/frc/team4131/robot/Robot.java
+++ src/org/usfirst/frc/team4131/robot/Robot.java
@@ -7,6 +7,7 @@
 
 package org.usfirst.frc.team4131.robot;
 
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
@@ -50,6 +51,8 @@ public class Robot extends IterativeRobot {
 
     @Override
     public void autonomousInit() {
+        String str = DriverStation.getInstance().getGameSpecificMessage();
+
         this.autoCommand = this.chooser.getSelected();
 
         if (this.autoCommand != null) {
