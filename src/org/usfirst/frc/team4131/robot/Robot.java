a1306910820f72f851316d657124c218032b0e22
Patrick Quam <pquam@hotmail.com>
2018-01-14 13:48:54 -0800

    More Camera tuning

--- src/org/usfirst/frc/team4131/robot/Robot.java
+++ src/org/usfirst/frc/team4131/robot/Robot.java
@@ -35,7 +35,7 @@ public class Robot extends IterativeRobot {
         DriveBaseSubsystem driveBase = new DriveBaseSubsystem();
 
         UsbCamera camera = CameraServer.getInstance().startAutomaticCapture();
-        camera.setVideoMode(new VideoMode(VideoMode.PixelFormat.kMJPEG, 700, 700, 10));
+        camera.setVideoMode(new VideoMode(VideoMode.PixelFormat.kMJPEG, 500, 500, 10));
 
         // TODO: Register auton Commands to chooser
         // this.chooser.addDefault("Default", new AutoDefaultCommand(driveBase));
