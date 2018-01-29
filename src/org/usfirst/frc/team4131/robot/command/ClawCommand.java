7c3df798ae2f81efe62c5ab57c3a928f83a36b68
Patrick Quam <pquam@hotmail.com>
2018-01-28 17:08:46 -0800

    Formatting - ClawCommand.java

--- src/org/usfirst/frc/team4131/robot/command/ClawCommand.java
+++ src/org/usfirst/frc/team4131/robot/command/ClawCommand.java
@@ -6,9 +6,11 @@ import org.usfirst.frc.team4131.robot.subsystem.ClawSubsystem;
  * A command which will perform a clamp action in order to
  * pick up a power cube.
  */
+
 // TODO: Complete
 public class ClawCommand extends SingleSubsystemCmd<ClawSubsystem> {
-    public ClawCommand(ClawSubsystem subsystem) {
+    
+	public ClawCommand(ClawSubsystem subsystem) {
         super(subsystem);
     }
 
