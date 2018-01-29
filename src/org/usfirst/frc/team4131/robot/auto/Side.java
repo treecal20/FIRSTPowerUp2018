2a9562e736c3a788d4aa6c3cec77415f356f74e8
Patrick Quam <pquam@hotmail.com>
2018-01-28 17:03:31 -0800

    Formatting - Side.java

--- src/org/usfirst/frc/team4131/robot/auto/Side.java
+++ src/org/usfirst/frc/team4131/robot/auto/Side.java
@@ -4,7 +4,8 @@ package org.usfirst.frc.team4131.robot.auto;
  * A unit of data sent by FMS
  */
 public enum Side {
-    LEFT, RIGHT;
+    
+	LEFT, RIGHT;
 
     public static Side decode(char c) {
         return c == 'L' ? LEFT : RIGHT;
