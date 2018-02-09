89d4f93b1f29afb86ac89db4c132e8b1fcf23962
Matthew Oberg <matthewroberg@gmail.com>
2018-02-08 17:00:28 -0800

    Fixes PID

--- src/org/usfirst/frc/team4131/robot/subsystem/DriveBaseSubsystem.java
+++ src/org/usfirst/frc/team4131/robot/subsystem/DriveBaseSubsystem.java
@@ -15,6 +15,8 @@ import org.usfirst.frc.team4131.robot.ctl.DriveCtl;
 import static org.usfirst.frc.team4131.robot.Oi.sigl;
 import static org.usfirst.frc.team4131.robot.Oi.sigr;
 
+import org.usfirst.frc.team4131.robot.Oi;
+
 /**
  * The drive base subsystem, linking the 4 Talon SRX
  * controllers and their respective motors.
@@ -32,7 +34,6 @@ public class DriveBaseSubsystem extends Subsystem implements PIDSource {
     // Physical drive mappings
     private final TalonSRX left;
     private final TalonSRX right;
-    private final TalonSRX right2;
 
     /**
      * Creates and caches the motors used for the drive base
@@ -44,14 +45,12 @@ public class DriveBaseSubsystem extends Subsystem implements PIDSource {
         new TalonSRX(RobotMap.L2).follow(this.left);
 
         this.right = new TalonSRX(RobotMap.R1);
-        this.right2 = new TalonSRX(RobotMap.R2);
-        this.right2.follow(this.right);
+        TalonSRX right2 = new TalonSRX(RobotMap.R2);
+        right2.follow(this.right);
 
         this.setupEncoder();
         this.reset();
         this.setupPid(this.left);
-        this.setupPid(this.right);
-        this.setupPid(this.right2);
     }
 
     @Override
@@ -100,10 +99,7 @@ public class DriveBaseSubsystem extends Subsystem implements PIDSource {
                 talon.config_kD(PID_IDX, 0, SENSOR_TIMEOUT),
                 talon.config_kF(PID_IDX, 0, SENSOR_TIMEOUT));
         ErrorCode code0 = ErrorCode.worstOne(talon.configAllowableClosedloopError(PID_IDX, ERROR_DELTA, SENSOR_TIMEOUT),
-                // talon.configVoltageCompSaturation(5, SENSOR_TIMEOUT),
-                talon.configClosedloopRamp(0.3, SENSOR_TIMEOUT));
-        // talon.configOpenloopRamp(0.3, SENSOR_TIMEOUT));
-        // talon.enableVoltageCompensation(true);
+                talon.configOpenloopRamp(0.3, SENSOR_TIMEOUT));
         if (code.value != 0 || code0.value != 0) {
             DriverStation.reportError("Error occurred configuring PIDF", false);
         }
@@ -123,6 +119,11 @@ public class DriveBaseSubsystem extends Subsystem implements PIDSource {
                     + " VOLTS=" + this.left.getBusVoltage());
         }
     }
+    
+    public void prepareTeleop() {
+    	this.left.configOpenloopRamp(0, SENSOR_TIMEOUT);
+    	this.right.configOpenloopRamp(0, SENSOR_TIMEOUT);
+    }
 
     // Talon control ---------------------------------------
 
@@ -138,19 +139,14 @@ public class DriveBaseSubsystem extends Subsystem implements PIDSource {
         r = r * r * r;
         this.left.set(ControlMode.PercentOutput, sigl() * l);
         this.right.set(ControlMode.PercentOutput, sigr() * r);
-        this.right2.set(ControlMode.PercentOutput, sigr() * r);
     }
-
-    /**
-     * Prepares the correct talon configuration in
-     * order to use the {@Link #gotoPosition(int)}
-     * method.
-     */
-    public void prepareAuto() {
-        this.right.follow(this.left);
-        this.right2.follow(this.left);
-        this.right.setInverted(true);
-        this.right2.setInverted(true);
+    
+    public void setVelocityLeft(double vel) {
+        this.left.set(ControlMode.PercentOutput, sigl() * vel);
+    }
+    
+    public void setVelocityRight(double vel) {
+        this.right.set(ControlMode.PercentOutput, sigr() * vel);
     }
 
     /**
@@ -162,26 +158,6 @@ public class DriveBaseSubsystem extends Subsystem implements PIDSource {
      */
     public void gotoPosition(int ticks) {
         this.left.set(ControlMode.Position, ticks);
-    }
-
-    /**
-     * Ends autonomous configuration on the talons,
-     * resets them so that teleop will work
-     * correctly.
-     */
-    public void prepareTeleop() {
-        this.right.set(ControlMode.PercentOutput, 0);
-        this.right2.follow(this.right);
-        this.right.setInverted(false);
-        this.right2.setInverted(false);
-    }
-
-    public void setVelocity(int vel) {
-        this.left.set(ControlMode.Velocity, sigl() * vel);
-    }
-
-    public void setVelocityLeft(int vel) {
-        this.left.set(ControlMode.Velocity, sigl() * vel);
     }
 
     // Sensor polling methods ------------------------------
@@ -202,18 +178,18 @@ public class DriveBaseSubsystem extends Subsystem implements PIDSource {
      * the left encoder
      */
     public int getLeftDist() {
-        return sigl() * this.left.getSelectedSensorPosition(PID_IDX);
+        return this.left.getSelectedSensorPosition(PID_IDX);
     }
 
     public int getRightDist() {
-        return sigr() * this.right.getSelectedSensorPosition(PID_IDX);
+        return this.right.getSelectedSensorPosition(PID_IDX);
     }
 
     // PID Fallback source methods -------------------------
 
     @Override
     public PIDSourceType getPIDSourceType() {
-        return PIDSourceType.kRate;
+        return PIDSourceType.kDisplacement;
     }
 
     @Override
@@ -223,6 +199,6 @@ public class DriveBaseSubsystem extends Subsystem implements PIDSource {
 
     @Override
     public double pidGet() {
-        return this.getLeftVelocity();
+        return this.getLeftDist();
     }
 }
